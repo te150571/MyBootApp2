4b4cd87860a7170562c9a9e5ca2e4d7088cdd232
渡邉 優 <150571@jc-21.jp>
2018-05-09 16:28:54 +0900

    Change file name from taro.java to HelloController.java

--- src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class taro
+public class HelloController
 {
 	@RequestMapping("/taro")
 	public String taro(){
