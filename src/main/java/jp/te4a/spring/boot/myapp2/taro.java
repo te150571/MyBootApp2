bc623132cc69fb59efab2d9a20e41130aea60a0f
渡邉 優 <150571@jc-21.jp>
2018-05-09 16:11:09 +0900

    First commit

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/taro.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp2;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class taro
+{
+	@RequestMapping("/taro")
+	public String taro(){
+		return "Taro desu!"; 
+	}
+}
